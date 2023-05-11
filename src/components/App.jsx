fa04f817e1971bf6f32a3fa45ef7dbe4b06274e5
HalynaSilina <halynasilina@gmail.com>
2023-05-11 13:36:46 +0300

    +

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
 import Searchbar from './Searchbar/Searchbar';
-import ImageGallery from './ImageGallery/ImageGallery';
-import Button from './Button/Button';
+// import ImageGallery from './ImageGallery/ImageGallery';
+// import Button from './Button/Button';
+// import Modal from './Modal/Modal';
 
 class App extends Component {
-  handleSubmit = () => {};
+  state = {
+    searchValue: '',
+  };
+
+  handleSubmit = value => {
+    this.setState({ searchValue: value });
+  };
+
+  // handleLoadMoreClick = () => {};
 
   render() {
     return (
       <>
         <Searchbar onSubmit={this.handleSubmit} />
-        <ImageGallery/>
-        <Button/>
+        {/* <ImageGallery /> */}
+        {/* <Button onClick={this.handleLoadMoreClick} /> */}
+        {/* <Modal /> */}
       </>
     );
   }
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,19 +1,36 @@
 import React, { Component } from 'react';
 
 class Searchbar extends Component {
+  state = {
+    searchValue: '',
+  };
+
+  handleSearchChange = evt => {
+    const searchValue = evt.currentTarget.value.trim().toLowerCase();
+    this.setState({ searchValue });
+  };
+
+  handleSubmit = evt => {
+    evt.preventDefault();
+    const { searchValue } = this.state;
+    this.props.onSubmit(searchValue);
+  };
+
   render() {
     return (
       <header class="searchbar">
-        <form class="form">
+        <form class="form" onSubmit={this.handleSubmit}>
           <button type="submit" class="button">
             <span class="button-label">Search</span>
           </button>
           <input
             class="input"
             type="text"
+            value={this.state.searchValue}
             autocomplete="off"
             autofocus
             placeholder="Search images and photos"
+            onChange={this.handleSearchChange}
           />
         </form>
       </header>
